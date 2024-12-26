93726872bbe65674e7da59ca458f91a2563d5cab
nicolheng <nicolhengsiyi@gmail.com>
2024-12-26 23:24:35 +0800

    delete an unused import

--- todolist/src/todolist/Email.java
+++ todolist/src/todolist/Email.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import javax.mail.Message;
 import javax.mail.MessagingException;
